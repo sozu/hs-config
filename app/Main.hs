a21cdb50fa73c0aefbcdfe75c0a5e69c4297f91e
sozu <sozu@github>
2018-10-03 17:36:31 +0900

    Add validation feature (not works for some field types)

--- app/Main.hs
+++ app/Main.hs
@@ -2,30 +2,34 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
+import Control.Monad
 import Data.Config
-import Data.Yaml
+import Data.Validation
 import Debug.Trace
 
-yamlConfiguration "data/th.yml" "Configuration" []
+$(yamlConfiguration "data/th.yml" "Configuration" [])
+$(validatable [''Configuration, ''Configuration'subType, ''Configuration'listing])
 
 main :: IO ()
 main = do
-    --structure <- parseYamlFile "data/test.yml" "Test"
-    --print structure
+    r <- loadYamlFile @Configuration' "data/th.yml"
+    case r of
+        Left c -> do
+            forM_ (errorsOf c) print
+        Right config -> do
+            print $ string config
+            print $ integer config
+            --print $ double config
+            print $ boolean config
+            print $ intlist config
 
-    config <- decodeFileThrow @_ @Configuration "data/th.yml" 
+            --print $ mapping config
+            print $ listing config
 
-    print $ string config
-    print $ integer config
-    print $ double config
-    print $ boolean config
-    print $ intlist config
-
-    print $ mapping config
-    print $ listing config
-
-    print $ st_string $ subType config
-    print $ st_integer $ subType config
+            print $ st_string $ subType config
+            print $ st_integer $ subType config
--- src/Data/Config.hs
+++ src/Data/Config.hs
@@ -1,14 +1,22 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Data.Config (
     module Data.Config.Structure
   , module Data.Config.TH
   , module Data.Config.Yaml.Parse
   , yamlConfiguration
+  , loadYamlFile
 ) where
 
 import Language.Haskell.TH
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Yaml
 import Data.Config.Structure
 import Data.Config.TH
 import Data.Config.Yaml.Parse
+import Data.Validation
 
 -- | Generates data types which represent the contents of YAML file.
 yamlConfiguration :: FilePath -- ^ Path to YAML.
@@ -17,4 +25,13 @@ yamlConfiguration :: FilePath -- ^ Path to YAML.
                   -> Q [Dec] -- ^ Declarations of data types.
 yamlConfiguration yaml n m = do
     s <- runIO $ parseYamlFile yaml n
-    defineDataTypes [] m s
+    defineDataTypes [] m s 
+
+loadYamlFile :: forall c m. (Validatable c, FromJSON c, MonadIO m)
+             => FilePath
+             -> m (Either c (ValidationTarget c))
+loadYamlFile path = do
+    config' <- decodeFileThrow @m @c path
+    return $ case validate config' of
+        Just config -> Right config
+        Nothing     -> Left config'
